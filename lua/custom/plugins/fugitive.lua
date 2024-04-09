506d08b93298373f01791e08396d6587f9b7a17b
Jack Herby <JacekZiolek@proton.me>
2024-04-09 13:34:59 +0200

    update fugitive keymaps

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -5,10 +5,10 @@ return {
     local keymap = vim.keymap
     keymap.set('n', '<leader>fc', ':Gdiffsplit!<CR>')
     keymap.set('n', '<leader>fs', ':Gdiffsplit<CR>')
-    keymap.set('n', '<leader>fh', 'do')
-    keymap.set('n', '<leader>fl', 'dp')
-    keymap.set('n', '<leader>f]', ']c') -- next diff hunk
-    keymap.set('n', '<leader>f[', '[c') -- previous diff hunk
+    keymap.set('n', 'fh', 'do')
+    keymap.set('n', 'fl', 'dp')
+    keymap.set('n', 'f]', ']c') -- next diff hunk
+    keymap.set('n', 'f[', '[c') -- previous diff hunk
     keymap.set('n', '<leader>fw', ':Gw!<Cr>')
     keymap.set('n', '<leader>fg', vim.cmd.Git)
     keymap.set('n', '<leader>fb', ':G blame<CR>')
